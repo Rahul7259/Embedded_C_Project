569bd90ed61891bce3d06349cd3298a624cee1ec
Rahul7259 <68542250+Rahul7259@users.noreply.github.com>
2021-04-26 13:48:35 +0530

    Delete Output.h

--- inc/Output.h
+++
@@ -1,9 +0,0 @@
-#ifndef OUTPUT_H_INCLUDED
-#define OUTPUT_H_INCLUDED
-#define LED_ON (0x01)
-#define LED_OFF (0x00)
-#include<avr/io.h>
-
-void change_led_state(uint8_t state);
-
-#endif // OUTPUT_H_INCLUDED
